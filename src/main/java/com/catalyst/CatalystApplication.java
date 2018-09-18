861be856b2914acf21b8671fd8a9384e7cbb9d25
prem85pal <prem85chandra@gmail.com>
2018-09-18 17:35:32 +0530

    initial commit

---
+++ src/main/java/com/catalyst/CatalystApplication.java
@@ -0,0 +1,12 @@
+package com.catalyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalystApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalystApplication.class, args);
+    }
+}
